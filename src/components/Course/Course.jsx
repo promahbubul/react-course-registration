be088e1b1c34c63b05b355234bd6b30b169e9363
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-09-14 21:29:59 +0600

    json data render

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -8,19 +8,18 @@ const Course = ({ courses }) => {
       {courses.map((course) => (
         <div key={course.id} className="p-4 bg-white	rounded-xl">
           <img src={course.cover_img} alt="" />
-          <h3 className="text-black text-lg font-semibold">{course.title}</h3>
-          <p className="text-gray">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.
-          </p>
+          <h3 className="text-black text-base font-semibold font-semibold my-4">
+            {course.title}
+          </h3>
+          <p className="text-gray">{course.desc}</p>
           <div className="flex justify-between mb-6 mt-4">
             <div className="flex">
               <FiDollarSign className="text-2xl" />
-              <span className="text-gray">Price: 15000</span>
+              <span className="text-gray">Price: {course.price}</span>
             </div>
             <div className="flex">
               <BiBookOpen className="text-2xl" />
-              <span className="text-gray">Credit : 1hr</span>
+              <span className="text-gray">Credit : {course.credit}hr</span>
             </div>
           </div>
           <button className="bg-blue text-center py-3 px-5  w-full  hover:bg-hoverBlue rounded-md mx-auto block text-white font-semibold text-lg">
